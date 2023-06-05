f18bdc2fbdba5152740b1cb22d6e507fc6b9ab1a
Azlan Arshad <132899069+azlanarshad90@users.noreply.github.com>
2023-06-05 15:33:05 +0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -5,32 +5,17 @@ from wtforms.validators import InputRequired
 from werkzeug.utils import secure_filename
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes.vectorstore import VectorstoreIndexCreator
 from langchain.chains.question_answering import load_qa_chain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
+from io import BytesIO
 import os
 
-tiktoken.encoding_for_model('gpt-3.5-turbo')
-tokenizer = tiktoken.get_encoding('cl100k_base')
 
-def tiktoken_len(text):
-    tokens = tokenizer.encode(
-        text,
-        disallowed_special=()
-    )
-    return len(tokens)
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=400,
-    chunk_overlap=20,
-    length_function=tiktoken_len,
-    separators=["\n\n", "\n", " ", ""]
-)
 app = Flask(__name__)
+
 
 app.secret_key = 'chatbot'
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
@@ -42,7 +27,6 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
 class UploadForm(FlaskForm):
     file = FileField('PDF File', validators=[InputRequired()])
     submit = SubmitField('Submit')
-
 
 @app.route('/',methods=["GET","POST"])
 def home():
@@ -62,21 +46,18 @@ def get_bot_response():
     loader = PyPDFLoader(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     index_creator = VectorstoreIndexCreator()
     docsearch = index_creator.from_loaders([loader])
-    chunks = text_splitter.split(loader.load())
-    answers = []
-    for chunk in chunks:
-        docs = docsearch.vectorstore.similarity_search(chunk)
-        prompt = f"""
+    docs = docsearch.vectorstore.similarity_search(userText)
+    prompt = f"""
         Answer the question that is given under triple round brackets with maximum detail.
         Make the answer maximum detailed with respect to your capability.
         Follow the format of the answer that is given in the question, if no information is provided, you can use the format you think is best equipped according to question.
-        ((({chunk})))
+        ((({userText})))
         """
-        answer = chain.run(input_documents=docs, question=prompt)
-        answers.append(answer)
-        return str(answers)
+    chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+    answer = chain.run(input_documents=docs, question=prompt)
+    return str(answer)
 
 
 if __name__ == "__main__":
     app.run()
-
+    
